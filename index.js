1f428d87fc0f21f95fdc8834d2191ce32a73373f
Basil Alosious <basilalosious@gmail.com>
2020-03-19 20:45:39 +0530

    adding database heroku , fixes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const knex = require('knex')
  const db=knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE.URL,
+    connectionString : process.env.DATABASE.URL,
     ssl:true,
     
   }
