efc2b27512dfd9e5285d09432c310c0c5364dcf5
Basil Alosious <basilalosious@gmail.com>
2020-03-19 20:34:02 +0530

    added heroku database

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const knex = require('knex')
  const db=knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-round-72654',
     user : 'postgres',
     password : 'basil',
     database : 'smartbrain'
